08b5665cb71936d8447b03549e6ca126eb81c92d
issing <issing@isger.net>
2017-04-10 20:54:03 +0800

    .

--- src/test/java/net/isger/brick/BrickRawGsonTest.java
+++ src/test/java/net/isger/brick/BrickRawGsonTest.java
@@ -6,6 +6,7 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import net.isger.raw.Depository;
+import net.isger.raw.StringRaw;
 
 public class BrickRawGsonTest extends TestCase {
 
@@ -21,8 +22,25 @@ public class BrickRawGsonTest extends TestCase {
         List<?> result = Depository.getArtifact("brick-modules.json")
                 .transform(List.class);
         System.out.println(result);
+        Hello hello = Depository.getArtifact(
+                new StringRaw("{'input_man' : 'test'}")).transform(Hello.class);
+        System.out.println(hello.getInput_man());
         assertTrue(result != null);
         assertTrue(result.size() == 1);
         assertEquals(result.get(0), "test");
+    }
+
+    public static class Hello {
+
+        private String input_man;
+
+        public String getInput_man() {
+            return input_man;
+        }
+
+        public void setInput_man(String input_man) {
+            this.input_man = input_man;
+        }
+
     }
 }
